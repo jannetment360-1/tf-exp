675e98c7a64bc2212525bb8167d4bf71806f65e3
jannetment360-1 <112550513+jannetment360-1@users.noreply.github.com>
2022-09-15 00:37:31 +1000

    Update initial_setup.sh

--- scripts/initial_setup.sh
+++ scripts/initial_setup.sh
@@ -1,5 +1,5 @@
 #1.create a project and export it into a variable(project creation- manual)
-export PROJECT_ID="mvp-liveability-230179"
+export PROJECT_ID="mvp-liveability-230181"
       
 #2.Sets the project,location &dataset
 gcloud config set project ${PROJECT_ID}
@@ -7,13 +7,35 @@ export LOCATION="australia-southeast1"
 export BQ_DATASET="liveability"
 export Git_Root="https://github.com/jannetment360-1/tf-exp"
 #3.Create the service account with same proj name and export it to a variable which can be used in later stages    
-#gcloud iam service-accounts create ${PROJECT_ID} \
-#	 --description="Service account for the Liveability project" \
-#	  --display-name="Liveability Service Account"    
+gcloud iam service-accounts create ${PROJECT_ID} \
+	 --description="Service account for the Liveability project" \
+	  --display-name="Liveability Service Account"    
 export SERVICE_ACCOUNT_ID=${PROJECT_ID}
-# 4.Assign different roles to the services account.
+#4.Enable all the APIs
+# service user to enable other APIs
+gcloud services enable servicemanagement.googleapis.com
+gcloud services enable servicecontrol.googleapis.com
+# dataflow 
+gcloud services enable dataflow.googleapis.com
+#firestore
+gcloud services enable firestore.googleapis.com
+#appengine for firestore
+gcloud services enable appengine.googleapis.com
+#cloud sql
+gcloud services enable sqladmin.googleapis.com
+#datastream
+gcloud services enable datastream.googleapis.com
+#pubsubapi
+gcloud services enable pubsub.googleapis.com
+#cloudbuild
+gcloud services enable cloudbuild.googleapis.com
+gcloud services enable compute.googleapis.com
+gcloud services enable iam.googleapis.com
+#5.Assign different roles to the services account.
 #common
 gcloud projects add-iam-policy-binding ${PROJECT_ID} --member=serviceAccount:${SERVICE_ACCOUNT_ID}@${PROJECT_ID}.iam.gserviceaccount.com --role=roles/iam.serviceAccountUser
+gcloud projects add-iam-policy-binding ${PROJECT_ID} --member=serviceAccount:${SERVICE_ACCOUNT_ID}@${PROJECT_ID}.iam.gserviceaccount.com --role=roles/iam.serviceAccountTokenCreator
+gcloud projects add-iam-policy-binding ${PROJECT_ID} --member=serviceAccount:${SERVICE_ACCOUNT_ID}@${PROJECT_ID}.iam.gserviceaccount.com --role=roles/iam.serviceAccountKeys.create
 #dataflow
 gcloud projects add-iam-policy-binding ${PROJECT_ID} --member=serviceAccount:${SERVICE_ACCOUNT_ID}@${PROJECT_ID}.iam.gserviceaccount.com --role=roles/dataflow.admin
 gcloud projects add-iam-policy-binding ${PROJECT_ID} --member=serviceAccount:${SERVICE_ACCOUNT_ID}@${PROJECT_ID}.iam.gserviceaccount.com --role=roles/bigquery.admin
@@ -37,50 +59,17 @@ gcloud projects add-iam-policy-binding ${PROJECT_ID} --member=serviceAccount:${S
 gcloud projects add-iam-policy-binding ${PROJECT_ID} --member=serviceAccount:${SERVICE_ACCOUNT_ID}@${PROJECT_ID}.iam.gserviceaccount.com --role=roles/logging.logWriter
 gcloud projects add-iam-policy-binding ${PROJECT_ID} --member=serviceAccount:${SERVICE_ACCOUNT_ID}@${PROJECT_ID}.iam.gserviceaccount.com --role=roles/workflows.invoker
 gcloud projects add-iam-policy-binding ${PROJECT_ID} --member=serviceAccount:${SERVICE_ACCOUNT_ID}@${PROJECT_ID}.iam.gserviceaccount.com --role=roles/compute.admin
-gcloud projects add-iam-policy-binding ${PROJECT_ID} --member=serviceAccount:${SERVICE_ACCOUNT_ID}@${PROJECT_ID}.iam.gserviceaccount.com --role=roles/iam.serviceAccountTokenCreator
 #5.Creating a service account key
 gcloud iam service-accounts keys create key.json --iam-account=${SERVICE_ACCOUNT_ID}@${PROJECT_ID}.iam.gserviceaccount.com
 export GOOGLE_APPLICATION_CREDENTIALS=key.json
-#6.Enable all the APIs
-# dataflow 
-gcloud services enable dataflow.googleapis.com
-#firestore
-gcloud services enable firestore.googleapis.com
-#appengine for firestore
-gcloud services enable appengine.googleapis.com
-#cloud sql
-gcloud services enable sqladmin.googleapis.com
-#datastream
-gcloud services enable datastream.googleapis.com
-#pubsubapi
-gcloud services enable pubsub.googleapis.com
-#cloudbuild
-gcloud services enable cloudbuild.googleapis.com
-gcloud services enable compute.googleapis.com
-gcloud services enable iam.googleapis.com
 #7.Create fire store DB for schema to be used in dataflow
 gcloud app create --region=${LOCATION}
 gcloud firestore databases create --region=${LOCATION}
 #8.Generates a GCS bucket with the project id.
 gsutil mb -l ${LOCATION} gs://${PROJECT_ID}
-    
-#9.Create a datset in bq
+#8.Create a datset in bq
 bq --location=${LOCATION} mk \
 	 --dataset ${BQ_DATASET} 
-#10.Clone the data and schema from git to cloud shell
-git clone ${Git_Root}
-#11. Copy the files to cloud storage
-gsutil cp ~/tf-exp/data/* gs://${PROJECT_ID}/data/batch_data/
-gsutil cp ~/tf-exp/ddl/* gs://${PROJECT_ID}/ddl/
-############################# For data flow start ########################################################
-#26.Create a directory
-#mkdir batch_data
-#cd batch_data
-#copy the json key of service account to current folder
-#cp ~/key.json .
-#cp ~/key.json ~/final_demo/scripts/key.json
-#cp ~/key.json ~/final_demo/dataflow/key.json
-#cp ~/key.json ~/final_demo/terraform/key.json
-#git add .
-#git commit -m "Key updated to the script"
-#git push
+#10. Copy the files to cloud storage
+gsutil cp ../data/* gs://${PROJECT_ID}/data/batch_data/
+gsutil cp ../ddl/* gs://${PROJECT_ID}/ddl/
