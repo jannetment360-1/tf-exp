8255e1ca50cd0e0668e9d62dfd8badc0bbeb6695
jannetment360-1 <112550513+jannetment360-1@users.noreply.github.com>
2022-09-14 16:28:50 +1000

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -33,18 +33,5 @@ resource "google_storage_bucket" "private-equity" {
   name          = "tf-exp-project-362208"
   location      = "australia-southeast1"
   force_destroy = true
-    }
-resource "google_service_account" "service_account" {
-  account_id   = "tfexp-sa"
-  display_name = "TFEXP Service Account"
 }
 
-resource "google_project_iam_binding" "sa_account_iam" {
-project = var.gcp_project
-count = length(var.rolesList)
-role =  var.rolesList[count.index]
-members = [
-  "serviceAccount:${google_service_account.service_account.email}"
-]
-depends_on = [google_service_account.service_account]
-}
