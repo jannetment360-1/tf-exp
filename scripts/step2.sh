c7d55ae1941033284d6d3025288c2e20d61775ec
jannetment360-1 <112550513+jannetment360-1@users.noreply.github.com>
2022-09-14 23:05:23 +1000

    Update step2.sh

--- scripts/step2.sh
+++ scripts/step2.sh
@@ -16,5 +16,5 @@ export SERVICE_ACCOUNT_ID=${PROJECT_ID}
 #10.Clone the data and schema from git to cloud shell
 #git clone ${Git_Root}
 #11. Copy the files to cloud storage
-gsutil cp ../tf-exp/data/* gs://${PROJECT_ID}/data/batch_data/
-gsutil cp ../tf-exp/ddl/* gs://${PROJECT_ID}/ddl/
+gsutil cp /home/jannetment3601/tf-exp/data/* gs://${PROJECT_ID}/data/batch_data/
+gsutil cp /home/jannetment3601/tf-exp/ddl/* gs://${PROJECT_ID}/ddl/
