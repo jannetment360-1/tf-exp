aeed5c3d7db6c1efaf6c98cc66c308a07a9d08ed
jannetment360-1 <112550513+jannetment360-1@users.noreply.github.com>
2022-09-15 23:19:48 +1000

    Update initial_setup.sh

--- scripts/initial_setup.sh
+++ scripts/initial_setup.sh
@@ -1,5 +1,5 @@
 #1.create a project and export it into a variable(project creation- manual)
-export PROJECT_ID="mvp-liveability-230182"
+export PROJECT_ID="mvp-liveability-230183"
       
 #2.Sets the project,location &dataset
 gcloud config set project ${PROJECT_ID}
